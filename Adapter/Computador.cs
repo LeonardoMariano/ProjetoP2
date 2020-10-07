c939b00940162822da1b2db007fefe314c64e20c
Leonardo Mariano <leonardo.h.mariano@hotmail.com>
2020-10-06 21:28:54 -0300

    Adapter Implementation

---
+++ Adapter/Computador.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoP2.Adapter
+{
+    public class Computador
+    {        
+        private string sistemaDeArquivos;
+        
+        public void setSistemaDeArquivos(string so)
+        {
+            this.sistemaDeArquivos = so;
+        }
+       
+        public string getSistemaDeArquivos()
+        {
+            return this.sistemaDeArquivos;
+        }
+        
+        public bool compativel(SistemaWindows windows)
+        {
+            return this.getSistemaDeArquivos() == windows.getSistemaDeArquivos();
+        }
+    }
+}
---
+++ Adapter/SistemaWindows.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoP2.Adapter
+{
+    public class SistemaWindows
+    {        
+        private string sistemaDeArquivos;
+
+        public SistemaWindows()
+        {
+            this.sistemaDeArquivos = "NTFS";
+        }
+
+        public void setSistemaDeArquivos(string sisArq)
+        {
+            this.sistemaDeArquivos = sisArq;
+        }
+
+        public virtual string getSistemaDeArquivos()
+        {
+            return this.sistemaDeArquivos;
+        }
+    }
+}
