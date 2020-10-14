7c6fc83ee3ebc144486e9f18d65e99799948373f
Leonardo Mariano <leonardo.h.mariano@hotmail.com>
2020-10-13 21:04:29 -0300

    Singleton Implementation

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using ProjetoP2.Adapter;
 using ProjetoP2.Facade;
+using ProjetoP2.Singleton;
 
 namespace ProjetoP2
 {
@@ -8,8 +9,18 @@ namespace ProjetoP2
     {
         static void Main(string[] args)
         {
+            //SINGLETON --------------------------------------------
+            Console.WriteLine("Creating database connection #1");
+            Database connection1 = Database.getConnection();
+            Console.ReadKey();
+
+            Console.WriteLine("Creating database connection #2");
+            Database connection2 = Database.getConnection();
+            Console.ReadKey();
+
+            //FACADE -----------------------------------------------
             ComputadorFacade computer = new ComputadorFacade();
-            computer.formatComputer();
+            computer.formatComputer();            
         }
     }
 }
---
+++ Singleton/Database.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoP2.Singleton
+{
+    public sealed class Database
+    {
+        private static Database connection = null;
+
+        private Database()
+        {
+
+        }
+
+        public static Database getConnection()
+        {
+
+            if (connection == null)
+            {
+                connection = new Database();
+                Console.WriteLine("Creating new connection...");
+            }
+            else
+            {
+                Console.WriteLine("Using existing connetion...");
+            }
+
+            return connection;
+        }
+    }
+}
